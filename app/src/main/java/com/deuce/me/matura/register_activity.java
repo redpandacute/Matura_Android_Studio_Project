0a90ad0fe4041517e64300faa30fd5f5c3beac2b
redpandacute <ingli292@gmail.com>
2018-01-14 20:27:01 +0100

    Nun auch register Activity umgeschrieben. Luaffähig.

--- app/src/main/java/com/deuce/me/matura/register_activity.java
+++ app/src/main/java/com/deuce/me/matura/register_activity.java
@@ -29,6 +29,48 @@ public class register_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_activity);
 
+        final Spinner yearofbirth_sp = findViewById(R.id.registeract_yearofbirth_spinner);
+        final Spinner school_sp = findViewById(R.id.registeract_school_spinner);
+
+        final Button registerbutton_bu = findViewById(R.id.registeract_signup_button);
+
+        SpinnerYears(yearofbirth_sp);
+
+        registerbutton_bu.setOnClickListener(new onRegisterListener());
+
+    }
+
+
+    //OnResponseListener Register ------------------------------------------------------------------
+    private class onResponseListener implements Response.Listener<String> {
+
+        @Override
+        public void onResponse(String response) {
+
+            try {
+                System.out.println(response);
+                JSONObject json_response = new JSONObject(response);
+                boolean success = json_response.getBoolean("success");
+
+                if (success) {
+                    Intent intent = new Intent(register_activity.this, login_activity.class); //if success, change to login screen
+                    register_activity.this.startActivity(intent);
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(register_activity.this);
+                    builder.setMessage("Ooops, something went wrong with your registration. Retry?").setNegativeButton("Retry", null).create().show(); //Error Message if something went
+                    // wrong with JSON Object, with
+                }                                                                                                                                                   // retry button.
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    //OnClickListener Register ---------------------------------------------------------------------
+    private class onRegisterListener implements View.OnClickListener {
+
         //Objects
         final EditText username_et = findViewById(R.id.registeract_username_edittext);
         final EditText name_et = findViewById(R.id.registeract_name_edittext);
@@ -42,9 +84,54 @@ public class register_activity extends AppCompatActivity {
 
         final CheckBox termsofservice_cb = findViewById(R.id.registeract_termsofservice_checkbox);
 
-        final Button registerbutton_bu = findViewById(R.id.registeract_signup_button);
+        @Override
+        public void onClick(View view) {
 
-        //yearofbirth Spinner adding Years
+            String username = username_et.getText().toString();
+            String name = name_et.getText().toString();
+            String surname = firstname_et.getText().toString();
+            String email = email_et.getText().toString();
+            String password = password_et.getText().toString();
+            String confpassword = confpassword_et.getText().toString();
+            String school = "";
+            int yearofbirth = (int) yearofbirth_sp.getSelectedItem();
+
+            try {
+                yearofbirth = Integer.parseInt(yearofbirth_sp.getSelectedItem().toString());
+                school = school_sp.getSelectedItem().toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (password.equals(confpassword)) {
+                if (termsofservice_cb.isChecked() && !username.isEmpty() && !name.isEmpty() && !surname.isEmpty() /*&& !school.isEmpty() */ && !email.isEmpty() && !password.isEmpty()){
+
+
+
+                    System.out.println("Creating request");
+                    //Creating Request
+                    register_request reg_request = new register_request(username, name, surname, school, yearofbirth,email, password, new onResponseListener());
+                    RequestQueue request_queue = Volley.newRequestQueue(register_activity.this); //Makeing a Requestqueue
+                    request_queue.add(reg_request);
+
+                } else {
+                    System.out.println("not accepting termsoS");
+                    //Error not accepting Terms o Service
+                }
+            } else {
+                System.out.println("confpw unsuccessful");
+                //Error if confpw was not successful
+            }
+
+
+        }
+    }
+
+
+    //Adding years function ------------------------------------------------------------------------
+    private void SpinnerYears (Spinner yearSpinner) {
+
+
         List years = new ArrayList();
         for (int i = Calendar.getInstance().get(Calendar.YEAR); i > Calendar.getInstance().get(Calendar.YEAR) - 100; i--){
             years.add(i);
@@ -54,76 +141,8 @@ public class register_activity extends AppCompatActivity {
         yearlist.addAll(years);
         final ArrayAdapter<Integer> year_spinner_adp = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, yearlist);
         year_spinner_adp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        yearofbirth_sp.setAdapter(year_spinner_adp);
+        yearSpinner.setAdapter(year_spinner_adp);
         year_spinner_adp.notifyDataSetChanged();
-
-
-        registerbutton_bu.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                String username = username_et.getText().toString();
-                String name = name_et.getText().toString();
-                String surname = firstname_et.getText().toString();
-                String email = email_et.getText().toString();
-                String password = password_et.getText().toString();
-                String confpassword = confpassword_et.getText().toString();
-                String school = "";
-                int yearofbirth = (int) yearofbirth_sp.getSelectedItem();
-
-                try {
-                    yearofbirth = Integer.parseInt(yearofbirth_sp.getSelectedItem().toString());
-                    school = school_sp.getSelectedItem().toString();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                if (password.equals(confpassword)) {
-                    if (termsofservice_cb.isChecked() && !username.isEmpty() && !name.isEmpty() && !surname.isEmpty() /*&& !school.isEmpty() */ && !email.isEmpty() && !password.isEmpty()){
-
-
-                        //ResponseListener
-                        Response.Listener<String> response_listener = new Response.Listener<String>(){
-
-                            @Override
-                            public void onResponse(String response) {
-                                try {
-                                    System.out.println(response);
-                                    JSONObject json_response = new JSONObject(response);
-                                    boolean success = json_response.getBoolean("success");
-
-                                    if (success) {
-                                        Intent intent = new Intent(register_activity.this, login_activity.class); //if success, change to login screen
-                                        register_activity.this.startActivity(intent);
-                                    } else {
-                                        AlertDialog.Builder builder = new AlertDialog.Builder(register_activity.this);
-                                        builder.setMessage("Ooops, something went wrong with your registration. Retry?").setNegativeButton("Retry", null).create().show(); //Error Message if something went
-                                                                                                                                                                                        // wrong with JSON Object, with
-                                    }                                                                                                                                                   // retry button.
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-
-                        };
-
-                        System.out.println("creating request");
-                        //Creating Request
-                        register_request reg_request = new register_request(username, name, surname, school, yearofbirth,email, password, response_listener);
-                        RequestQueue request_queue = Volley.newRequestQueue(register_activity.this); //Makeing a Requestqueue
-                        request_queue.add(reg_request);
-
-                    } else {
-                        System.out.println("not accepting termsoS");
-                        //Error not accepting Terms o Service
-                    }
-                } else {
-                    System.out.println("confpw unsuccessful");
-                    //Error if confpw was not successful
-                }
-
-            }
-        });
 
     }
 }
