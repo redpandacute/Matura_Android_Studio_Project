612f12475ae6f1d3fea5721f6b566895a8b66097
redpandacute <ingli292@gmail.com>
2018-01-15 20:41:40 +0100

    Savesettings in Klasse unterteilt. Problem aufgefallen mit PHP skript, werde es nun verbessern.

--- app/src/main/java/com/deuce/me/matura/settings_activity.java
+++ app/src/main/java/com/deuce/me/matura/settings_activity.java
@@ -76,139 +76,156 @@ public class settings_activity extends AppCompatActivity {
             music_cb.setChecked((boolean) extras_bundle.get("subj_music"));
             chemics_cb.setChecked((boolean) extras_bundle.get("subj_chemics"));
             physics_cb.setChecked((boolean) extras_bundle.get("subj_physics"));
+
+            savechanges_bt.setOnClickListener(new onSaveListener());
         } else {
             //Error with extras
             id = 0;
         }
+    }
 
-        savechanges_bt.setOnClickListener(new View.OnClickListener() {
+    class onSaveListener implements View.OnClickListener {
 
-            @Override
-            public void onClick(View view) {
+        Bundle extras_bundle = getIntent().getExtras();
+        int id = (int) extras_bundle.get("user_id");
 
+        final EditText firstname_et = (EditText) findViewById(R.id.settingsact_firstname_edittext);
+        final EditText name_et = (EditText) findViewById(R.id.settingsact_name_edittext);
+        final EditText email_et = (EditText) findViewById(R.id.settingsact_email_edittext);
+        final EditText description_et = (EditText) findViewById(R.id.settingsact_description_edittext);
+        final EditText oldpassword_et = (EditText) findViewById(R.id.settingsact_oldpassword_edittext);
+        final EditText newpassword_et = (EditText) findViewById(R.id.settingsact_newpassword_edittext);
+        final EditText newconfpassword_et = (EditText) findViewById(R.id.settingsact_confnewpassword_edittext);
 
+        final Spinner school_sp = (Spinner) findViewById(R.id.settingsact_school_spinner);
 
-                String name = name_et.getText().toString();
-                String surname = firstname_et.getText().toString();
-                String email = email_et.getText().toString();
-                String description = description_et.getText().toString();
+        final CheckBox german_cb = (CheckBox) findViewById(R.id.settingsact_german_checkbox);
+        final CheckBox spanish_cb = (CheckBox) findViewById(R.id.settingsact_spanish_checkbox);
+        final CheckBox english_cb = (CheckBox) findViewById(R.id.settingsact_english_checkbox);
+        final CheckBox french_cb = (CheckBox) findViewById(R.id.settingsact_french_checkbox);
+        final CheckBox biology_cb = (CheckBox) findViewById(R.id.settingsact_biology_checkbox);
+        final CheckBox music_cb = (CheckBox) findViewById(R.id.settingsact_music_checkbox);
+        final CheckBox chemics_cb = (CheckBox) findViewById(R.id.settingsact_chemics_checkbox);
+        final CheckBox maths_cb = (CheckBox) findViewById(R.id.settingsact_maths_checkbox);
+        final CheckBox physics_cb = (CheckBox) findViewById(R.id.settingsact_physics_checkbox);
 
-                // String school = school_sp.getSelectedItem().toString();
-                String school = "";
+        @Override
+        public void onClick(View view) {
 
-                boolean german = german_cb.isChecked();
-                boolean spanish = spanish_cb.isChecked();
-                boolean english = english_cb.isChecked();
-                boolean french = french_cb.isChecked();
-                boolean biology = biology_cb.isChecked();
-                boolean chemics = chemics_cb.isChecked();
-                boolean maths = maths_cb.isChecked();
-                boolean physics = physics_cb.isChecked();
-                boolean music = music_cb.isChecked();
+            String name = name_et.getText().toString();
+            String surname = firstname_et.getText().toString();
+            String email = email_et.getText().toString();
+            String description = description_et.getText().toString();
 
-                if(newpassword_et.getText().toString().isEmpty()) {
+            // String school = school_sp.getSelectedItem().toString();
+            String school = "";
 
-                    //TODO no pw request
+            boolean german = german_cb.isChecked();
+            boolean spanish = spanish_cb.isChecked();
+            boolean english = english_cb.isChecked();
+            boolean french = french_cb.isChecked();
+            boolean biology = biology_cb.isChecked();
+            boolean chemics = chemics_cb.isChecked();
+            boolean maths = maths_cb.isChecked();
+            boolean physics = physics_cb.isChecked();
+            boolean music = music_cb.isChecked();
 
-                } else if(newpassword_et.getText().toString().equals(newconfpassword_et.getText().toString()) && !newpassword_et.getText().toString().isEmpty() && !newconfpassword_et.getText().toString().isEmpty() ) {
+            if(newpassword_et.getText().toString().isEmpty()) {
 
-                    String oldpassword = oldpassword_et.getText().toString();
-                    String newpassword = newpassword_et.getText().toString();
+                //TODO no pw request
 
-                    Response.Listener<String> response_listener = new Response.Listener<String>() {
+            } else if(newpassword_et.getText().toString().equals(newconfpassword_et.getText().toString()) && !newpassword_et.getText().toString().isEmpty() && !newconfpassword_et.getText().toString().isEmpty() ) {
 
-                        @Override
-                        public void onResponse(String response) {
-
-                            try {
-                                System.out.println("Received Response: " + response);
-                                JSONObject json_response = new JSONObject(response);
-                                boolean success = json_response.getBoolean("success");
-
-                                System.out.println("Success: " + success);
-
-                                if (success) {
-
-                                    //Retreiving data from response
-                                    String user_username = json_response.getString("user_username");
-                                    String user_name = json_response.getString("user_name");
-                                    String user_firstname = json_response.getString("user_firstname");
-                                    String user_school = json_response.getString("user_school");
-                                    String user_email = json_response.getString("user_email");
-
-                                    String user_description = "";
-
-                                    try {
-                                        user_description = json_response.getString("user_description");
-                                    } catch (Exception e) {
-                                        e.printStackTrace();
-                                        System.out.println("Warning: No Description");
-                                    }
-
-                                    int user_id = json_response.getInt("user_id");
-
-                                    boolean subj_german = json_response.getBoolean("subj_german");
-                                    boolean subj_spanish = json_response.getBoolean("subj_spanish");
-                                    boolean subj_english = json_response.getBoolean("subj_english");
-                                    boolean subj_french = json_response.getBoolean("subj_french");
-                                    boolean subj_music = json_response.getBoolean("subj_music");
-                                    boolean subj_chemics = json_response.getBoolean("subj_chemics");
-                                    boolean subj_biology = json_response.getBoolean("subj_biology");
-                                    boolean subj_physics = json_response.getBoolean("subj_physics");
-                                    boolean subj_maths = json_response.getBoolean("subj_chemics");
-
-                                    Intent save_intent = new Intent(settings_activity.this, mainpage_activity.class);
-
-                                    save_intent.putExtra("user_username", user_username);
-                                    save_intent.putExtra("user_name", user_name);
-                                    save_intent.putExtra("user_firstname", user_firstname);
-                                    save_intent.putExtra("user_username", user_name);
-                                    save_intent.putExtra("user_school", user_school);
-                                    save_intent.putExtra("user_email", user_email);
-                                    save_intent.putExtra("user_description", user_description);
-                                    save_intent.putExtra("user_id", user_id);
-
-                                    save_intent.putExtra("subj_german", subj_german);
-                                    save_intent.putExtra("subj_french", subj_french);
-                                    save_intent.putExtra("subj_english", subj_english);
-                                    save_intent.putExtra("subj_music", subj_music);
-                                    save_intent.putExtra("subj_spanish", subj_spanish);
-                                    save_intent.putExtra("subj_maths", subj_maths);
-                                    save_intent.putExtra("subj_biology", subj_biology);
-                                    save_intent.putExtra("subj_chemics", subj_chemics);
-                                    save_intent.putExtra("subj_physics", subj_physics);
-
-                                    startActivity(save_intent);
-                                }
+                String oldpassword = oldpassword_et.getText().toString();
+                String newpassword = newpassword_et.getText().toString();
 
 
+                System.out.println("Making save request");
 
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
+                savesettings_pw_request save_request = new savesettings_pw_request(id, surname, name, email, school, description, oldpassword, newpassword, german, spanish, english, french, biology, chemics, music, maths, physics, new onResponseListener());
+                RequestQueue request_queue = Volley.newRequestQueue(settings_activity.this); //Request Queue
+                request_queue.add(save_request);
 
-                        }
+            } else {
 
-                    };
+                //TODO conf pw failed
+                System.out.println("Not inserted Correctly");
+            }
+        }
+    }
 
-                    System.out.println("Making save request");
+    class onResponseListener implements Response.Listener<String> {
 
-                    savesettings_pw_request save_request = new savesettings_pw_request(id, surname, name, email, school, description, oldpassword, newpassword,
-                                                                                        german, spanish, english, french, biology, chemics, music, maths, physics,
-                                                                                        response_listener);
-                    RequestQueue request_queue = Volley.newRequestQueue(settings_activity.this); //Request Queue
-                    request_queue.add(save_request);
+        @Override
+        public void onResponse(String response) {
 
-                } else {
+            try {
+                System.out.println("Received Response: " + response);
+                JSONObject json_response = new JSONObject(response);
+                boolean success = json_response.getBoolean("success");
 
-                    //TODO conf pw failed
-                    System.out.println("Not inserted Correctly");
+                System.out.println("Success: " + success);
+
+                if (success) {
+
+                    //Retreiving data from response
+                    String user_username = json_response.getString("user_username");
+                    String user_name = json_response.getString("user_name");
+                    String user_firstname = json_response.getString("user_firstname");
+                    String user_school = json_response.getString("user_school");
+                    String user_email = json_response.getString("user_email");
+
+                    String user_description = "";
+
+                    try {
+                        user_description = json_response.getString("user_description");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.out.println("Warning: No Description");
+                    }
+
+                    int user_id = json_response.getInt("user_id");
+
+                    boolean subj_german = json_response.getBoolean("subj_german");
+                    boolean subj_spanish = json_response.getBoolean("subj_spanish");
+                    boolean subj_english = json_response.getBoolean("subj_english");
+                    boolean subj_french = json_response.getBoolean("subj_french");
+                    boolean subj_music = json_response.getBoolean("subj_music");
+                    boolean subj_chemics = json_response.getBoolean("subj_chemics");
+                    boolean subj_biology = json_response.getBoolean("subj_biology");
+                    boolean subj_physics = json_response.getBoolean("subj_physics");
+                    boolean subj_maths = json_response.getBoolean("subj_chemics");
+
+                    Intent save_intent = new Intent(settings_activity.this, mainpage_activity.class);
+
+                    save_intent.putExtra("user_username", user_username);
+                    save_intent.putExtra("user_name", user_name);
+                    save_intent.putExtra("user_firstname", user_firstname);
+                    save_intent.putExtra("user_username", user_name);
+                    save_intent.putExtra("user_school", user_school);
+                    save_intent.putExtra("user_email", user_email);
+                    save_intent.putExtra("user_description", user_description);
+                    save_intent.putExtra("user_id", user_id);
+
+                    save_intent.putExtra("subj_german", subj_german);
+                    save_intent.putExtra("subj_french", subj_french);
+                    save_intent.putExtra("subj_english", subj_english);
+                    save_intent.putExtra("subj_music", subj_music);
+                    save_intent.putExtra("subj_spanish", subj_spanish);
+                    save_intent.putExtra("subj_maths", subj_maths);
+                    save_intent.putExtra("subj_biology", subj_biology);
+                    save_intent.putExtra("subj_chemics", subj_chemics);
+                    save_intent.putExtra("subj_physics", subj_physics);
+
+                    startActivity(save_intent);
                 }
 
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        });
-
-
-
+        }
     }
 }
+
+
+
